1fcd9ea7820b281ae17137e121f1734b6cc89e25
Harland Hauger <harlandhauger@gmail.com>
2014-09-25 14:25:22 -0700

    initial commit, homepage routed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThermicWhims::Application.config.secret_key_base = '403f0151722822bef8b0c47466d2d10e3b428f629a9e672d9b07b9911361e5cde07baf35fe7b67c2c3f901f486791205064638e7a4d709b004c0f499dd4c7068'
