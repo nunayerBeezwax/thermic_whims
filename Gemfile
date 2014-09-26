4c3f329a9bb2f5381d4344fb73a8326c4136141f
Harland Hauger <harlandhauger@gmail.com>
2014-09-26 13:33:40 -0700

    pagination gem wired up

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'devise'
 
+gem 'will_paginate', '~> 3.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,7 @@
 class VideosController < ApplicationController
 
 	def index
-		@videos = Video.all
+		@videos = Video.paginate(:page => params[:page], :per_page => 2)
 	end
 
 end
