0ed9f06f2ba21bcdce747d5d27696883bce682b2
Harland Hauger <harlandhauger@gmail.com>
2014-09-26 14:11:10 -0700

    added construct_embed_url to remove youtube formatting

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,7 @@ class Video < ActiveRecord::Base
 	# has_many :fountains
 	# has_many :memes, through: :fountains
 
+	def construct_embed_url
+		"http://" + self.url + "?hd=1&rel=0&autohide=1&showinfo=0"
+	end
 end
