8a3b996938072fc9e960eafd1098e0727a080b30
Harland Hauger <harlandhauger@gmail.com>
2014-09-30 10:38:21 -0700

    moving line

--- config/application.rb
+++ config/application.rb
@@ -7,14 +7,13 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-config.serve_static_assets = true
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ThermicWhims
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
