594fe8a950171f375f2a75724f911f39eccd52e5
Harland Hauger <harlandhauger@gmail.com>
2014-10-07 11:28:02 -0700

    validation and homepage css

--- app/models/meme.rb
+++ app/models/meme.rb
@@ -2,6 +2,8 @@ class Meme < ActiveRecord::Base
 
 	acts_as_taggable
 
+	validates_presence_of :fountain_ids, :text
+
 	has_and_belongs_to_many :fountains
 	has_and_belongs_to_many :videos
 	has_and_belongs_to_many :tags
